a5a0beb1ea6e3d76299a5bf969b64426d2782fb7
rmeggins <rmeggins>
2006-09-29 17:17:19 +0000

    bump version to 1.0.3

--- src/com/netscape/management/client/console/Console.java
+++ src/com/netscape/management/client/console/Console.java
@@ -56,7 +56,7 @@ public class Console implements CommClient {
     // preference information
     //
     public static final String IDENTIFIER = "Console";
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.0.3";
     public static final String MAJOR_VERSION = "1.0";
     public static final String PREFERENCES_LOGIN =
             IDENTIFIER + "." + VERSION + ".Login.preferences";
