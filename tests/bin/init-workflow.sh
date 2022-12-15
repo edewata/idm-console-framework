2ffba13aa284cf16429a20f2987393574e965f66
Endi S. Dewata <edewata@redhat.com>
2022-12-15 08:22:30 +0700

    Add initialization job

---
+++ tests/bin/init-workflow.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -e
+
+if [ "$BASE64_MATRIX" == "" ]
+then
+    MATRIX="{\"os\":[\"latest\"]}"
+else
+    MATRIX=$(echo "$BASE64_MATRIX" | base64 -d)
+fi
+
+echo "MATRIX: $MATRIX"
+echo "matrix=$MATRIX" >> $GITHUB_OUTPUT
+
+if [ "$BASE64_REPO" == "" ]
+then
+    REPO="@pki/master"
+else
+    REPO=$(echo "$BASE64_REPO" | base64 -d)
+fi
+
+echo "REPO: $REPO"
+echo "repo=$REPO" >> $GITHUB_OUTPUT
